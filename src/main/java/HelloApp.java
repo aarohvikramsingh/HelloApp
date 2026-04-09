d4b2270ffd00997c6c41049881b337e5b0025214
Aaroh Vikram Singh <aarohvikramsingh@gmail.com>
2026-04-09 20:10:59 +0530

    Coded UC7: Display Hello with multiple args using String.join() method

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -3,17 +3,8 @@ public class HelloApp {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            StringBuilder names = new StringBuilder();
-
-            // Append every name with a comma and space
-            for (String name : args) {
-                names.append(name).append(", ");
-            }
-
-            // Remove the final trailing comma and space (the last 2 characters)
-            String finalNames = names.substring(0, names.length() - 2);
-
-            System.out.println("Hello, " + finalNames + "!");
+            String names = String.join(", ", args);
+            System.out.println("Hello, " + names + "!");
         }
     }
 }
