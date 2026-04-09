aacd72d74880e2eba21df7affa84d250e6de4b7c
Aaroh Vikram Singh <aarohvikramsingh@gmail.com>
2026-04-09 22:30:48 +0530

    Coded UC1

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,10 +1,5 @@
 public class HelloApp {
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("Hello, World!");
-        } else {
-            String names = String.join(", ", args);
-            System.out.println("Hello, " + names + "!");
-        }
+        System.out.println("Hello World");
     }
 }
